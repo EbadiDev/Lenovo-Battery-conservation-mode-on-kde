7ad8c4a8d7240f04eb717be0c104f04848e82dd1
EbadiDev <kingali0098@gmail.com>
2023-04-30 10:57:32 +0330

    VPC finder

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Lenovo Battery conservation mode on kde Plasma
 So I was looking for KDE Plasma widget to control Conservation mode something like gnome extenstion [IdeaPad](https://extensions.gnome.org/extension/2992/ideapad/) but didn't find any so i made this on Qt6 and I was to lazy to learn how to make KDE Plasma widget hope someone find this and make it widget for KDE Plasma (or i will do it)
+
+Don't worry about your different lenovo model with this, it searches for filenames containing "VPC" in the ideapad_acpi directory and uses the first one found (if any) as the new mode_file. If no files containing "VPC" are found, an error message is printed and the function returns.
 # How to run  
 
 ```
--- main.py
+++ main.py
@@ -26,12 +26,24 @@ class Window(QWidget):
             os.system("sudo chmod 666 " + mode_file)
 
     def change_mode(self, state):
+        
+        mode_file = "/sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode"
+        #checks for VPC in the filename if VPC2004:00 doesn't exist 
+        if not os.path.isfile(mode_file):
+            files = [f for f in os.listdir("/sys/bus/platform/drivers/ideapad_acpi/") if "VPC" in f]
+            if len(files) > 0:
+                mode_file = "/sys/bus/platform/drivers/ideapad_acpi/" + files[0] + "/conservation_mode"
+            else:
+                print("Error: could not find conservation_mode file for Lenovo laptop")
+                return
+
         if state == 2:
-            command = 'sudo echo 1 > /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode'
+            command = 'sudo echo 1 > ' + mode_file
             os.system(command)
         else:
-            command = 'sudo echo 0 > /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode'
+            command = 'sudo echo 0 > ' + mode_file
             os.system(command)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
